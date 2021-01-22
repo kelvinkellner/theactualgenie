5bb489c2486bafd258549bd8b202bf1a1a855024
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-01-21 22:14:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# monke
+
+A Discord bot that replies with a picture of a monkey.
+Made to annoy a friend, of course, as all good things are.
+
+Add this bot to your own server:
+https://discord.com/oauth2/authorize?client_id=801994211288678420&scope=bot
+
+I can't guarantee this bot will work as expected.
+I am hosting this bot on a free Heroku dyno and the probably that I will exceed the free data limit if you add this to your server too is decent.
---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const genies = ['1.jpg','2.png','3.png','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.png','10.png','11.jpg','12.jpg','13.png','14.png','15.jpg','16.jpg','17.jpg','18.jpg'];
+const users = {};
+
+const token = process.env.BOT_TOKEN; //set to: process.env.BOT_TOKEN for Heroku deployment, set to: config.LOCAL_TOKEN for local testing
+
+client.on('ready', () => {
+    console.log('Genie up!');
+});
+
+client.on('message', message => { //this event is fired, whenever the bot sees a new message
+    try {
+        if(message.content.toLowerCase().includes('i wish')) {
+            if(message.content.toLowerCase().includes('more wishes')) {
+                const id = message.author.id;
+                if(id in users) {
+                    if(users[id] == 0) {
+                        users[id] += 3;
+                        message.reply('of course, dear. you have been granted 3 additional wishes for a total of ' + users[message.author.id]);
+                    } else {
+                        users[id] += 3;
+                        message.reply('consider yourself lucky. you have been granted 3 additional wishes for a total of ' + users[message.author.id]);
+                    }
+                } else {
+                    users[id] = 3;
+                }
+            } else {
+                if(message.author.id in users && users[message.author.id]!=0) {
+                    users[message.author.id] -= 1;
+                    message.reply('your wish has been granted!');
+                } else {
+                    message.reply('you do not have any wishes, my friend.');
+                }
+            }
+            message.channel.send('',{files: ['./assets/' + genies[Math.floor(Math.random() * array.length)]]});
+        }
+    } catch(err) {
+        console.log(err.message);
+    }
+    
+});
+
+process.on('unhandledRejection', error => {
+	console.error('Unhandled promise rejection:', error);
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(token); // BOT_TOKEN is the Client Secret
