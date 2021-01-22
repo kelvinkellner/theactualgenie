f3fe5b9dca90c8b7594eaf76ed1ee33f337b23bb
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-01-21 22:50:34 -0500

    Replies contain image, not two separate replies

--- bot.js
+++ bot.js
@@ -13,29 +13,30 @@ client.on('ready', () => {
 client.on('message', message => { //this event is fired, whenever the bot sees a new message
     try {
         if(message.content.toLowerCase().includes('i wish')) {
+            let text = 'whoops! blame the programmer, cuz something is wrong.'
             if(message.content.toLowerCase().includes('wishes')) {
                 const id = message.author.id;
                 if(id in users && users[id] > 0) {
                     users[id] += 3;
-                    message.reply('consider yourself lucky. you have been granted 3 additional wishes for a total of ' + users[message.author.id] + '.');
+                    text = 'consider yourself lucky. you have been granted 3 additional wishes for a total of ' + users[message.author.id] + '.';
                 } else {
                     users[id] = 3;
-                    message.reply('of course dear. you have been granted 3 wishes.');
+                    text = 'of course dear. you have been granted 3 wishes.';
                 }
             } else {
                 if(message.author.id in users) {
                     if(users[message.author.id] == 0) {
-                        message.reply('you do not have enough wishes, my friend.');
+                        text = 'you do not have enough wishes, my friend.';
                     } else {
                         users[message.author.id] -= 1;
-                        message.reply('your wish has been granted!');
+                        text = 'your wish has been granted!';
                     }
                 } else {
                     users[message.author.id] = 0;
-                    message.reply('your wish has been granted!');
+                    text = 'your wish has been granted!';
                 }
             }
-            message.channel.send('',{files: ['./assets/' + genies[Math.floor(Math.random() * genies.length)]]});
+            message.reply(text,{files: ['./assets/' + genies[Math.floor(Math.random() * genies.length)]]});
         }
     } catch(err) {
         console.log(err.message);
